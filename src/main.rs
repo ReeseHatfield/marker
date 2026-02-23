9a49934cffa02149f62ab2b8adba81b10eedc3f7
ReeseHatfield <reesehatfield02@gmail.com>
2026-02-22 21:59:59 -0500

    plan

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,20 @@
+
+/// sends a request
+///
+/// @param url string
+/// @param retries int = 3 number of retries
+/// @param timeout ms = 5000 request timeout
+/// @return type description
+
+
+/// description
+/// @param <name> <type> [= default_val] [description]
+/// @return <type> description
+
+
+
+//type can be unioned with [sdf|]
+
 fn main() {
     println!("Hello, world!");
 }
